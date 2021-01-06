70ffccdc0fb7badcf760f84d66b893c368156e92
kinjalrami <kinjal.rami19@gmail.com>
2021-01-06 17:09:55 -0600

    Options added

--- src/test/java/com/CarGurus/TestCase_006.java
+++ src/test/java/com/CarGurus/TestCase_006.java
@@ -13,8 +13,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  *
@@ -40,6 +42,16 @@ public class TestCase_006 {
         System.setProperty("webdriver.chrome.driver", "C:\\qa\\driver\\chromedriver.exe");//location of chromedriver defined here
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NONE); // https://www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
+        options.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
+        options.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
+        //options.addArguments("--headless"); // only if you are ACTUALLY running headless
+        options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+        options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+        options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+        options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+        options.addArguments("--disable-gpu");
         driver.get("https://www.cargurus.com/");
         //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
@@ -55,12 +67,12 @@ public class TestCase_006 {
     //
     @Test
     public void changeLanguage() throws Exception {
-        
+
         driver.findElement(By.id("location-menu-control")).click();
         driver.findElement(By.partialLinkText("Canada (F")).click();
-        
+
         String t = driver.findElement(By.xpath("//body/nav[@id='headerNav']/div[1]/ul[2]/li[2]/button[1]/span[1]")).getText();
-        System.out.println("Language: "+t);
+        System.out.println("Language: " + t);
         try {
             assertEquals("FR", t);
             System.out.println("Tes case Pass");
